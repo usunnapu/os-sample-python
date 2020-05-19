8d2ca32afe988b1d40b1511d9a440aa378cf61d9
umesh sunnapu <usunnapu>
2020-05-18 23:05:37 -0400

    fixed application name from app to application

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, url_for, redirect, render_template
 import requests
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
 def generate_query_example_url(language):
@@ -11,11 +11,11 @@ def generate_query_example_url(language):
 def display(response):
     print('response: {}'.format(response.text))
 
-@app.route("/")
+@application.route("/")
 def hello():
     return "Hello This is MS1!"
 
-@app.route('/language', methods=["POST", "GET"])
+@application.route('/language', methods=["POST", "GET"])
 def login():
     if request.method == "POST":
         language = request.form.get('language')
@@ -26,7 +26,7 @@ def login():
     else:
         return render_template("language.html")
 
-@app.route("/<lang>")
+@application.route("/<lang>")
 def language(lang):
     return f"<h1>{lang}</h1>"
 
