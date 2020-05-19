073ba1ba7524d01c6181f0153817671c15e5eda7
umesh sunnapu <usunnapu>
2020-05-18 23:16:56 -0400

    this is ms2

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, request
+
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello this is MS2!"
+
+@application.route('/query_example')
+def query_example():
+    language = request.args.get('language')
+    print('language is: {}'.format(language))
+    return 'MS2 -> MS1: {}'.format(language)
 
 if __name__ == "__main__":
-    application.run()
+    application.run(debug=True)
