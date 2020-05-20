b00b3fd8b6ec6c6469b21eb70b4df1a463430660
umesh sunnapu <usunnapu>
2020-05-19 20:16:01 -0400

    fixed typo port given as string

--- wsgi.py
+++ wsgi.py
@@ -8,7 +8,7 @@ application = Flask(__name__)
 def send_message_to_vran(msg):
     conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print('establishing connect to vran service url port 80')
-    conn.connect(('vran.app.svc.cluster.local','80'))
+    conn.connect(('vran.app.svc.cluster.local',80))
     print('connected to vran service url port 80')
     conn.sendall(msg)
     print('sent msg: {}'.format(msg))
