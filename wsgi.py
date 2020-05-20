15d36ee2d6f9e9b192d2e3a00ebd6636108b8385
umesh sunnapu <usunnapu>
2020-05-19 20:02:53 -0400

    connect to vran

--- wsgi.py
+++ wsgi.py
@@ -1,34 +1,31 @@
 from flask import Flask, request, url_for, redirect, render_template
 import requests
+import socket
 
 application = Flask(__name__)
 
 
-def generate_query_example_url(language):
-    url = 'http://100.82.38.31:6001/query_example?language={}'.format(language)
-    return url
-
-def display(response):
-    print('response: {}'.format(response.text))
+def send_message_to_vran(msg):
+    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print('establishing connect to vran service url port 80')
+    conn.connect(('vran.app.svc.cluster.local','80'))
+    print('connected to vran service url port 80')
+    conn.sendall(msg)
+    print('sent msg: {}'.format(msg))
+    conn.close()
 
 @application.route("/")
 def hello():
-    return "Hello This is MS1!"
+    return "Hello This is User Input Home Page"
 
 @application.route('/language', methods=["POST", "GET"])
-def login():
+def language():
     if request.method == "POST":
         language = request.form.get('language')
-        url = generate_query_example_url(language)
-        response = requests.get(url)
-        display(response)
-        return redirect(url_for("language", lang=language))
+        send_message_to_vran(language)
+        return f'<h1> Input: {} </h1>'.format(language)
     else:
         return render_template("language.html")
-
-@application.route("/<lang>")
-def language(lang):
-    return f"<h1>{lang}</h1>"
 
 if __name__ == "__main__":
     application.run(debug=True, port=5000)
