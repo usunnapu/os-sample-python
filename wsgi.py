acf02864af729b1e67f3e150b70c5cb94c72b69b
usunnapu <40170140+usunnapu@users.noreply.github.com>
2020-05-20 00:46:22 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,34 +1,16 @@
-from flask import Flask, request, url_for, redirect, render_template
-import requests
+from flask import Flask, request
 
 application = Flask(__name__)
 
-
-def generate_query_example_url(language):
-    url = 'http://100.82.38.31:6001/query_example?language={}'.format(language)
-    return url
-
-def display(response):
-    print('response: {}'.format(response.text))
-
 @application.route("/")
 def hello():
-    return "Hello This is MS1!"
+    return "Hello this is MS1!"
 
-@application.route('/language', methods=["POST", "GET"])
-def login():
-    if request.method == "POST":
-        language = request.form.get('language')
-        url = generate_query_example_url(language)
-        response = requests.get(url)
-        display(response)
-        return redirect(url_for("language", lang=language))
-    else:
-        return render_template("language.html")
-
-@application.route("/<lang>")
-def language(lang):
-    return f"<h1>{lang}</h1>"
+@application.route('/ms1_query')
+def ms1_query():
+    language = request.args.get('language')
+    print('language is: {}'.format(language))
+    return 'PYUSER -> MS1: {}'.format(language)
 
 if __name__ == "__main__":
-    application.run(debug=True, port=5000)
+    application.run(debug=True)
